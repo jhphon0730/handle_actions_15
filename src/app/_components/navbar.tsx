7b864af2ce96408554346aff03c46a75c23e8b65
Naithar01 <eeung025@gmail.com>
2025-10-28 23:02:43 +0900

    feat : 메인 로고 추가  - Navbar에 로고 추가 및 텍스트 변경  - 모바일 버전의 Padding 수정

--- src/app/_components/navbar.tsx
+++ src/app/_components/navbar.tsx
@@ -1,18 +1,25 @@
 import Link from "next/link"
+import Image from "next/image"
 
 export const Navbar = () => {
   return (
     <header className="fixed top-0 left-0 w-full bg-white px-6 py-4">
       <nav className="flex items-center justify-between w-full">
         <h1 className="font-bold text-xl uppercase">
-          <Link href="/">Todo</Link>
+          <Link href="/" className="inline-flex items-center gap-2">
+            <Image
+              src="/mainBanner.svg"
+              alt="Logo"
+              width={32}
+              height={0}
+              className="object-contain"
+            />
+            <p>Fucking TODO List</p>
+          </Link>
         </h1>
 
         {/* 네비게이션 링크 */}
         <ul>
-          <li className="inline-block mr-4">
-            <Link href="/">Home</Link>
-          </li>
           <li className="inline-block">
             <Link href="/category">Categories</Link>
           </li>
