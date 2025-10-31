80908846ca3f281b512ebf2e8290997059809097
Naithar01 <eeung025@gmail.com>
2025-10-31 21:38:02 +0900

    feat : Todo 데이터를 볼 수 있는 Table 추가  * 현재 Header, Body를 간단히 볼 수 있도록 추가

--- src/app/_components/todo-table.tsx
+++ src/app/_components/todo-table.tsx
@@ -1,5 +1,5 @@
 import { Spinner } from "@/components/ui/spinner";
-import { Table } from "@/components/ui/table";
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
 
 import { Todo } from "@/lib/types";
 
@@ -21,15 +21,48 @@ export const TodoTable = async ({ todos, isOutOfRange }: TodoTableProps) => {
   }
 
   return (
-    <div className="flex flex-col gap-4 mt-4">
+    <div className="flex flex-col gap-4 mt-4 rounded-md border">
     {/* 전체 목록 */}
     {/* 검색 및 필터링 / Toolbar */}
 
     {/* 데이터 테이블 */}
     <Table>
-      {/* Header 컴포넌트 */}
-
-      {/* Body 컴포넌트 */}
+      <TableHeader>
+        <TableRow>
+          {/* TODO : TableHeadCell.tsx 추가 */}
+          <TableHead className="w-[100px] font-bold"><div>Todo</div></TableHead>
+          <TableHead className="w-full font-bold"><div>Title</div></TableHead>
+          <TableHead className="w-[100px] font-bold"><div>Status</div></TableHead>
+          <TableHead className="w-[100px] font-bold"><div>Create At</div></TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {todos.length ? (
+          todos.map((todo) => (
+            <TableRow>
+              <TableCell>
+                Todo-{todo.id.slice(-5)}
+              </TableCell>
+              <TableCell>
+                {todo.title}
+              </TableCell>
+              <TableCell>
+                {todo.done ? "Done" : "Pending" }
+              </TableCell>
+              <TableCell>
+                {todo.createdAt.toLocaleString("ko-KR", { timeZone: "UTC" })}
+              </TableCell>
+            </TableRow>
+          ))
+        ) : (
+          <TableRow>
+            <TableCell
+              colSpan={4}
+              className="h-24 text-center"
+            >No Todos.</TableCell>
+          </TableRow>
+        )}
+      </TableBody>
     </Table>
 
     </div>
