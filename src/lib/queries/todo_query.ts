acd65ccd0a6449a45792428a8ab6b4e983c20771
jhphon0730 <eeung025@gmail.com>
2025-11-14 17:42:59 +0900

    code : 사용하지 않는 함수 제거

--- src/lib/queries/todo_query.ts
+++ src/lib/queries/todo_query.ts
@@ -1,24 +1,7 @@
 import { db } from "@/lib/db";
 
-import { Todo } from "@/lib/types";
-
-type getTodosQueryResult = {
-  todos: Todo[];
-  totalCount: number;
-}
-
 export const getTodoHeaders = async (): Promise<string[]> => {
   const todo = await db.todo.findFirst();
   return todo ? Object.keys(todo) : [];
 }
 
-export const getTodosQuery = async (): Promise<getTodosQueryResult> => {
-  const [todos, totalCount] = await Promise.all([
-    db.todo.findMany({
-      orderBy: { createdAt: "desc" },
-    }),
-    db.todo.count()
-  ]);
-  
-  return { todos, totalCount };
-}
