49c0b4bbf9529d7891d7c4ae937660108bedb6b7
Naithar01 <eeung025@gmail.com>
2025-11-18 20:57:06 +0900

    feat : Todo 테이블에서 Column으로 데이터를 정렬할 수 있도록 수정

--- src/app/_components/todo/todo-table-cell.tsx
+++ src/app/_components/todo/todo-table-cell.tsx
@@ -1,65 +1,103 @@
+import { ChevronsUpDown, ArrowUp, ArrowDown } from "lucide-react";
+
 import { Button } from "@/components/ui/button";
 import { TableHead, TableCell } from "@/components/ui/table";
-import { DropdownMenu, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
-import { cn, FirstChildCharUpper } from "@/lib/utils";
 import type { Todo } from "@/lib/types";
+import { useTodoStore } from "@/store/useTodoStore";
+import { cn, FirstChildCharUpper } from "@/lib/utils";
 
 /* 공통 셀 타입 */
 type BaseCellProps = React.HTMLAttributes<HTMLDivElement> & {
   cellData: string;
-}
+};
 
 /* 헤더 셀 */
-export const TodoTableHeadCell = ({ cellData, className, ...props }: BaseCellProps) => {
+export const TodoTableHeadCell = ({
+  cellData,
+  className,
+  ...props
+}: BaseCellProps) => {
   const customClass = cellData === "title" ? "w-full" : "w-[300px]";
-  const customClassByHead = cellData === "title" ? "flex items-center gap-2" : "w-[150px]"
+  const customClassByHead =
+    cellData === "title" ? "flex items-center gap-2" : "w-[150px]";
   cellData = cellData === "id" ? "#" : cellData;
 
   /* 정렬이 가능한 컬럼 식별 */
-  const isSortable = cellData === "title" || cellData === "status" || cellData === "priority";
+  const isSortable =
+    cellData === "title" || cellData === "status" || cellData === "priority";
 
   return (
     <TableHead
       {...props}
-      className={cn(className, customClass, "font-semibold") }
+      className={cn(className, customClass, "font-semibold")}
     >
       <div className={customClassByHead}>
         {/* ID의 경우 "#"으로 변경, 첫 글자만 대문자 -> 소문자로 유지 */}
-        { !isSortable ? FirstChildCharUpper(cellData) : (
-          <SortableHeader 
-            label={cellData}
-          />
+        {!isSortable ? (
+          FirstChildCharUpper(cellData)
+        ) : (
+          <SortableHeader label={cellData} />
         )}
       </div>
     </TableHead>
-    
   );
 };
 
 type SortableHeaderProps = {
   label: string;
-}
+};
 
 /* Title 헤더 셀 정렬 */
-const SortableHeader = ({label}: SortableHeaderProps) => {
-  // <button onClick={() => {onSort(label as keyof Todo, "asc")}}>{label}</button>
+const SortableHeader = ({ label }: SortableHeaderProps) => {
+  const { sortableColumn, handleSortByColumn } = useTodoStore();
+
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button type="button" variant="ghost" className="px-0 font-semibold gap-2">
+        <Button type="button" variant="ghost" className="font-semibold gap-2">
           <span>{FirstChildCharUpper(label)}</span>
+          {sortableColumn.column && sortableColumn.column === label ? (
+            sortableColumn.order == "asc" ? (
+              <ArrowUp className="h-4 w-4" />
+            ) : (
+              <ArrowDown className="h-4 w-4" />
+            )
+          ) : (
+            <ChevronsUpDown className="h-4 w-4" />
+          )}
         </Button>
       </DropdownMenuTrigger>
+      <DropdownMenuContent align="start">
+        <DropdownMenuGroup>
+          <DropdownMenuItem
+            className="cursor-pointer"
+            onClick={() => handleSortByColumn(label as keyof Todo, "asc")}
+          >
+            <ArrowUp className="h-4 w-4" />
+            Asc
+          </DropdownMenuItem>
+          <DropdownMenuItem
+            className="cursor-pointer"
+            onClick={() => handleSortByColumn(label as keyof Todo, "desc")}
+          >
+            <ArrowDown className="h-4 w-4" />
+            Desc
+          </DropdownMenuItem>
+        </DropdownMenuGroup>
+      </DropdownMenuContent>
     </DropdownMenu>
   );
-}
+};
 
 /* 바디 셀 */
 export const TodoTableBodyCell = ({ cellData, ...props }: BaseCellProps) => {
-  return (
-    <TableCell>
-      {cellData}
-    </TableCell>
-  );
+  return <TableCell>{cellData}</TableCell>;
 };
